384b5285b26799b7830f540717de0550c3e3a4a5
Spheniscine <spheniscine@gmail.com>
2026-03-19 15:15:50 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+
+// string inclusion is used to prevent FOUC
+const MAIN_CSS: &str = const_css_minify::minify!("../assets/main.css");
+const TAILWIND_CSS: &str = const_css_minify::minify!("../assets/tailwind.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Style { {MAIN_CSS} }
+        document::Style { {TAILWIND_CSS} }
+        Hero {}
+
+    }
+}
+
+#[component]
+pub fn Hero() -> Element {
+    rsx! {
+        div {
+            id: "hero",
+            img { src: HEADER_SVG, id: "header" }
+            div { id: "links",
+                a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
+                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
+                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            }
+        }
+    }
+}
