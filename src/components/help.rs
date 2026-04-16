08cbdf8e10a4c26990735249ce30e1f81bfb3832
Spheniscine <spheniscine@gmail.com>
2026-04-16 16:28:56 +0800

    add some text

--- src/components/help.rs
+++ src/components/help.rs
@@ -16,14 +16,23 @@ pub fn Help() -> Element {
     
     rsx! {
         div {
-            style: "overflow: hidden; position: absolute; height: 40rem; width: 40rem; border: 0.5rem solid #fff",
+            style: "padding: 2rem; display: flex; flex-direction: column; align-items: center;",
+            div {
+                style: "overflow: hidden; position: relative; top: 2rem; height: 40rem; width: 40rem; border: 0.5rem solid #fff",
 
-            HexGrid { 
-                board,
-                origin,
-                scale,
-                dim_blocked: true,
+                HexGrid { 
+                    board,
+                    origin,
+                    scale,
+                    dim_blocked: true,
+                },
             },
+
+            div {
+                style: "position: relative; top: 4rem; font-size: 4.5rem; color: #fff; text-align: center;",
+                "Your goal is to clear the board. Select a free orb, then match it with another orb, such that their numbers add up to ten, to remove both orbs from the board."
+            }
         }
+        
     }
 }
