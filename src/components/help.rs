6f8e121e4b8cf40cd6be3d70fec909d6b8a831ae
Spheniscine <spheniscine@gmail.com>
2026-04-16 20:42:52 +0800

    clean up positioning math and add copy of board

--- src/components/help.rs
+++ src/components/help.rs
@@ -1,32 +1,57 @@
 use dioxus::prelude::*;
-use hexx::{Hex, storage::HexagonalMap, Vec2};
+use hexx::{Hex, Vec2, hex, storage::HexagonalMap};
 
 use crate::{components::HexGrid, game::Board};
 
 #[component]
 pub fn Help() -> Element {
-    let origin = Vec2 { x: 20., y: 14. };
-    let scale = 7.;
+    let view_scale: f32 = 40.;
+    let hex_scale: f32 = 7.;
+    let hex_aspect_ratio: f32 = 0.8660254;
+    let origin = Vec2::splat(view_scale / 2.) - Vec2 { x: 0., y: hex_scale * hex_aspect_ratio };
 
-    let mut board = Board {
+    let mut board1 = Board {
         inner: HexagonalMap::new(Hex::ORIGIN, 3, |_| None)
     };
-    board[Hex::ORIGIN] = Some(3);
-    board[Hex { x: 0, y: 1}] = Some(7);
+    board1[Hex::ORIGIN] = Some(3);
+    board1[Hex { x: 0, y: 1}] = Some(7);
+
+    let mut board2 = board1.clone();
     
     rsx! {
         div {
             style: "padding: 2rem; display: flex; flex-direction: column; align-items: center;",
-            div {
-                style: "overflow: hidden; position: relative; top: 2rem; height: 40rem; width: 40rem; border: 0.5rem solid #fff",
 
-                HexGrid { 
-                    board,
-                    origin,
-                    scale,
-                    dim_blocked: true,
+            div {
+                style: "display: flex; flex-direction: row;",
+
+                div {
+                    style: "overflow: hidden; position: relative; top: 2rem; height: {view_scale}rem; width: {view_scale}rem;",
+
+                    HexGrid { 
+                        board: board1,
+                        origin,
+                        scale: hex_scale,
+                        dim_blocked: true,
+                    },
                 },
-            },
+
+                div {
+                    style: "width: 5rem;"
+                }
+
+                div {
+                    style: "overflow: hidden; position: relative; top: 2rem; height: {view_scale}rem; width: {view_scale}rem;",
+
+                    HexGrid { 
+                        board: board2,
+                        origin,
+                        scale: hex_scale,
+                        dim_blocked: true,
+                        selected: Hex::ORIGIN,
+                    },
+                },
+            }
 
             div {
                 style: "position: relative; top: 4rem; font-size: 4.5rem; color: #fff; text-align: center;",
