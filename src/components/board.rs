62e5346361aebb0864daa82185046c4e8ee173c7
Spheniscine <spheniscine@gmail.com>
2026-04-20 20:41:28 +0800

    adjust board and add copyright notice

--- src/components/board.rs
+++ src/components/board.rs
@@ -7,7 +7,7 @@ use crate::{components::HexGrid, game::GameState};
 pub fn BoardComponent(
     state: Signal<GameState>,
 ) -> Element {
-    let origin = Vec2 { x: 50., y: 72. };
+    let origin = Vec2 { x: 50., y: 71.2 };
     let st = state();
     let scale = 49. / (1. + 1.5 * st.board.inner.bounds().radius as f32);
     let onclick= move |hex| {
